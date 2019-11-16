f784c11e16eb9328cd38761517800f512aa898d9
Gabe <gabesolo@umich.edu>
2019-11-16 15:29:05 -0500

    Updates

---
+++ precache-manifest.c35b672a22a1d55a428b46dbc1fe88d2.js
@@ -0,0 +1,38 @@
+self.__precacheManifest = (self.__precacheManifest || []).concat([
+  {
+    "revision": "6ae865d49dc06fbc841f658822edc588",
+    "url": "/connectfour/index.html"
+  },
+  {
+    "revision": "59ae0b74fd89e3210a82",
+    "url": "/connectfour/static/css/main.af2e298b.chunk.css"
+  },
+  {
+    "revision": "a0b1248b7011b08d0c67",
+    "url": "/connectfour/static/js/2.bffa428b.chunk.js"
+  },
+  {
+    "revision": "59ae0b74fd89e3210a82",
+    "url": "/connectfour/static/js/main.d4940c26.chunk.js"
+  },
+  {
+    "revision": "a9d295dd93a3510005c6",
+    "url": "/connectfour/static/js/runtime-main.8c290396.js"
+  },
+  {
+    "revision": "dc0b480fe65ad504e8cccfbe9931f9e7",
+    "url": "/connectfour/static/media/red.dc0b480f.png"
+  },
+  {
+    "revision": "a044fc796c87950794be43067e7657c3",
+    "url": "/connectfour/static/media/redwin.a044fc79.png"
+  },
+  {
+    "revision": "bb219dcb84457b89592ab8b2fb3241b8",
+    "url": "/connectfour/static/media/yellow.bb219dcb.png"
+  },
+  {
+    "revision": "c182a9e0fdb61df0fca32e8f8c1c5246",
+    "url": "/connectfour/static/media/yellowwin.c182a9e0.png"
+  }
+]);
